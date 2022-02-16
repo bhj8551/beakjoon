ab90e79e62be0a6b3e9205045ccd1e59288c7445
bae hong jik <63235354+bhj8551@users.noreply.github.com>
2022-02-16 23:53:41 +0900

    1874

--- 1874.py
+++ 1874.py
@@ -3,20 +3,25 @@ from sys import stdin
 N = int(stdin.readline())
 stack = [0]
 operations = []
+max = 0
 for n in range(N):
     s = int(stdin.readline())
-
     if stack[-1] < s:
-        for i in range(stack[-1] + 1, s):
+        for i in range(max + 1, s):
             stack.append(i)
             operations.append('+')
+        operations.append('+')
         operations.append('-')
+        max = s
     elif s == stack[-1]:
         stack.pop()
         operations.append('-')
     else:
         operations = 'NO'
         break
-    print('stack', stack)
-    print('operations', operations)
-print(operations)
+
+if operations[0] == 'N':
+    print(operations)
+else:
+    for i in range(len(operations)):
+        print(operations[i])
